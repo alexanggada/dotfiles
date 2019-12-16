a4e2fa9760ad960ad6ae01d8693599a875d39a70
alexanggada <alexpanggada@gmail.com>
2019-12-16 09:58:03 +0700

    added symbolic link to conf files

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# nvim
+ln -s /Users/alexanderanggada/arithmox/dotfiles/nvim/init.vim /Users/alexanderanggada/.config/nvim
+
+# tmux
+ln -s /Users/alexanderanggada/arithmox/dotfiles/tmux/.tmux.conf /Users/alexanderanggada/.tmux.conf
+
+# zsh
+ln -s /Users/alexanderanggada/arithmox/dotfiles/zsh/.zshrc /Users/alexanderanggada/.zshrc
